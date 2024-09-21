15fb6ee5d824297eb0b53cd852decd7c7af58644
Ototsky <of9494of9494@gmail.com>
2024-09-21 14:49:01 +0300

    adding the phone menu slider

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+const burgerBtn = document.querySelector(".burger-menu");
+const phoneMenu = document.querySelector(".phone-menu");
+const overlay = document.querySelector("#overlay");
+const closeBtn = document.querySelector(".close");
+
+burgerBtn.addEventListener("click", () => {
+  phoneMenu.classList.add("active");
+  overlay.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  phoneMenu.classList.remove("active");
+  overlay.classList.remove("active");
+});
