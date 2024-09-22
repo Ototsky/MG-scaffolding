c8f3a1681565d265e9532d718c66bc68e716631a
Ototsky <of9494of9494@gmail.com>
2024-09-22 12:50:07 +0300

    editing phone menu behaviour

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,13 @@ const burgerBtn = document.querySelector(".burger-menu");
 const phoneMenu = document.querySelector(".phone-menu");
 const overlay = document.querySelector("#overlay");
 const closeBtn = document.querySelector(".close");
-const preloader = document.querySelector('.preloader');
+const preloader = document.querySelector(".preloader");
+const phoneMenuBtns = document.querySelectorAll(".phone-ul li");
 
 window.onload = function () {
-  preloader.style.visibility = 'hidden';
-  preloader.style.display = 'none';
-}
+  preloader.style.visibility = "hidden";
+  preloader.style.display = "none";
+};
 
 burgerBtn.addEventListener("click", () => {
   phoneMenu.classList.add("active");
@@ -19,3 +20,9 @@ closeBtn.addEventListener("click", () => {
   overlay.classList.remove("active");
 });
 
+phoneMenuBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    phoneMenu.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
